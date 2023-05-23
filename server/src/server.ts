8065977489ea70f5e303b0a8c89d616eb91c780e
Felipe Menegueli <felipemenegueli08@gmail.com>
2023-05-23 17:43:22 -0300

    Adicionando o server, nosso backend em node

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+const app = fastify()
+
+app.get('/users', () => {
+  const users = prisma.user.findMany()
+
+  return users
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on http://localhost:3333')
+  })
